dd41028c850d05d5b34da0adbf8fd1d4d85d93d6
Jaime J Cobo <jaimecobo@outlook.com>
2022-05-06 16:11:25 -0500

    components

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,22 @@
 
 export default function Home() {
   return (
-  <div className={styles.wrapper}>
+  <div className={style.wrapper}>
+    {/* <Header/> */}
 
+    {registered ? (
+      <div className={style.homewrapper}>
+        {/* <sidebar/> */}
+        <div className={style.main}>
+            {/* <Feed/> */}
+        </div>  
+        {/* <RightSidebar/> */}
+      </div>
+    ):(
+      <div className={style.signupContainer}>
+        {/* <Signup/> */}
+      </div>
+    )}
   </div>
   )
 }
